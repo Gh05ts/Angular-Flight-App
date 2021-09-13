feac7bd4e43b7ffe2342e3ff2c48d36ada887641
luv <luv.vermani@gmail.com>
2021-09-14 04:59:39 +0530

    routes setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateFlightComponent } from './create-flight/create-flight.component';
+import { DeleteFlightComponent } from './delete-flight/delete-flight.component';
+import { ModifyFlightComponent } from './modify-flight/modify-flight.component';
+import { ViewFlightComponent } from './view-flight/view-flight.component';
+
+const routes: Routes = [
+  { path: 'createFlight', component: CreateFlightComponent },
+  { path: 'modifyFlight', component: ModifyFlightComponent },
+  { path: 'deleteFlight', component: DeleteFlightComponent },
+  { path: 'viewFlight', component: ViewFlightComponent }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
---
+++ src/app/modify-flight/modify-flight.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-flight',
+  templateUrl: './modify-flight.component.html',
+  styleUrls: ['./modify-flight.component.css']
+})
+export class ModifyFlightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
