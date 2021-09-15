d0fd71e73ab3ffe6fc25cd56a2cf2d1eb993a1c2
luv <luv.vermani@gmail.com>
2021-09-15 09:14:59 +0530

    create init

--- src/app/create-flight/create-flight.component.ts
+++ src/app/create-flight/create-flight.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateFlightComponent implements OnInit {
 
+  providerNames = [
+    "Indigo",
+    "SpiceJet",
+    "Air Asia",
+    "Go Air",
+    "Jet Airways",
+    "Air India"
+  ]
+
+  providerTypes = ["Domestic", "International"]
+
+  providerCode: string = "6E-"
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  firstDropChanged(val: any) {
+    const airline = val.target.value
+    switch (airline) {
+      case "Indigo":
+        this.providerCode = "6E-"
+        break
+      case "SpiceJet":
+        this.providerCode = "SG-"
+        break
+      case "Air Asia":
+        this.providerCode = "I5-"
+        break
+      case "Go Air":
+        this.providerCode = "G8-"
+        break
+      case "Jet Airways":
+        this.providerCode = "9W-"
+        break
+      case "Air India":
+        this.providerCode = "AI-"
+        break
+    }
   }
 
 }
